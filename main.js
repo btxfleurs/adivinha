3d34e3640b005d125e22a4e83ee9c031270a985b
btxfleurs <101543322+btxfleurs@users.noreply.github.com>
2022-09-30 00:10:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var numeroSecreto = parseInt(Math.random() * 50);
+var botaoChute = document.getElementById("chutar");
+var botaoJogar = document.getElementById("jogar");
+var valor = document.getElementById("valor");
+var secao = document.getElementById("flex");
+
+botaoJogar.addEventListener("click", function() {
+  location.reload();
+});
+
+function Chutar() {
+  var resultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+  
+  if(chute == numeroSecreto){
+    resultado.innerHTML = "Parabéns, você acertou";
+    botaoChute.style.display = 'none';
+    valor.style.display = 'none';
+    botaoJogar.style.display = 'block';
+    secao.style.gap = '0';
+
+  } else if (chute > 50 || chute < 0) {
+    window.alert("Valor Inválido! Você deve digitar um número de 0 a 50");
+  } else if(chute > numeroSecreto){
+    resultado.innerHTML = "Que pena, você errou, o número secreto é menor que " + chute + ". Tente novamente!";
+  } else {
+    resultado.innerHTML = "Que pena, você errou, o número secreto é maior que " + chute + ". Tente novamente!";
+  }
+}
